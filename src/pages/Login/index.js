1adeb165bb4d40651079cb63eb6d0e90694bc13a
Gustavo616 <gustavolavrasmg@hotmail.com>
2021-10-15 15:57:48 -0300

    1.2.2

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,20 @@
+import '../Login/style.css'
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../../assets/logo.svg"
+import heroes from "../../assets/heroes.png"
+import voltar_login from "../../assets/voltar_login.svg"
 
 export default function Login(){
     return(
     <div className="Wraper">
-        <Link to='/register' >Registrar</Link>
+        <img className="logo" src={logo} alt=""/>
+        <h1>Fassa seu Login</h1>
+        <input type="text" placeholder="Sua ID" ></input>
+        <button type="submit" >Cadastro</button>
+        <img className="voltar_login" src={voltar_login} alt=""/>
+        <Link to='/register' >Não Tenho Cadastro</Link>
+        <img className="heroes" src={heroes} alt=""/>
     </div>
     );
 }
